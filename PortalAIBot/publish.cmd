ac35bbf210c22784377de2f2bb70464a6da7f221
Petr Draxler <pdraxler-cw@starbucks.com>
2018-06-20 08:02:50 -0700

    TechPortalBOT

---
+++ PortalAIBot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=portalaibot-Web-Deploy.pubxml -p:Password=dG6qWF1rAfB50hj7mYALSJrNcjv0AolXtZW5juwjzrBuQW5rzm08kgGDRmeM
+
--- TechPortalBOT/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=techportalbot-Web-Deploy.pubxml -p:Password=39maqscLZnEwJb6gpmkD2H2K7EyDxEhNfurJKXj2Xo91aJkilkLuxK60ag5f
-
